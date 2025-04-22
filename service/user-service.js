a4a063c0aaf67af8e7252c89ee59c29789541627
Alexei ZHeryakov <az1773443@gmail.com>
2025-04-22 19:23:44 +0700

    Initial commit

---
+++ service/user-service.js
@@ -0,0 +1,32 @@
+import bcrypt from "bcrypt";
+import { v4 as uuidv4 } from "uuid";
+import UserDto from "../dtos/user-dto.js";
+import UserModel from "../models/user-model.js";
+import mailService from "./mail-service.js";
+import tokenService from "./token-service.js";
+
+class UserService {
+  async registration(email, password) {
+    const candidate = await UserModel.findOne({ email });
+    if (candidate) {
+      throw new Error(`Пользователь с почтой ${email} уже существует`);
+    }
+    const hashPassword = await bcrypt.hash(password, 3);
+    const activationLink = uuidv4();
+    const user = await UserModel.create({
+      email,
+      password: hashPassword,
+      activationLink,
+    });
+    await mailService.sendActivationMail(
+      email,
+      `${process.env.API_URL}/api/activate/${activationLink}`
+    );
+    const userDto = new UserDto(user);
+    const tokens = tokenService.generateTokens({ ...userDto });
+    await tokenService.saveToken(userDto.id, tokens.refreshToken);
+    return { ...tokens, user: userDto };
+  }
+}
+
+export default new UserService();
