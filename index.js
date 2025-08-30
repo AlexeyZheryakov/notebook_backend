5668b83722ea50e6c595488eb8608cb5489044bd
Alexei ZHeryakov <az1773443@gmail.com>
2025-08-31 01:25:39 +0700

    fix CORS

--- index.js
+++ index.js
@@ -13,7 +13,14 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
+app.use(
+  cors({
+    origin: [process.env.CLIENT_URL, "http://localhost:3000"],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
 app.use("/api", router);
 app.use(errorMiddleware);
 
